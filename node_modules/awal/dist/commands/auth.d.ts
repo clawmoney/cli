cc3e20c7d122de2ad9533cc6ee95ba15eed81040
Jack Lee <280147597@qq.com>
2026-03-23 16:33:36 +0800

    chore: add .gitignore, remove node_modules

--- node_modules/awal/dist/commands/auth.d.ts
+++
@@ -1,3 +0,0 @@
-import { Command } from 'commander';
-export declare function createAuthCommand(): Command;
-//# sourceMappingURL=auth.d.ts.map
--- node_modules/awal/dist/commands/status.d.ts
+++
@@ -1,3 +0,0 @@
-import { Command } from 'commander';
-export declare function createStatusCommand(): Command;
-//# sourceMappingURL=status.d.ts.map
